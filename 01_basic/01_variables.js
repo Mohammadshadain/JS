abe4031abe3b018d1b0590e31278bf9f91e81a20
Mohammadshadain <shadain044714@gmail.com>
2025-03-13 08:28:08 +0530

    first commit

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,11 @@
+const myname = "shadain"
+let food="biryani"
+var number =9129801663
+food = "nihari"
+number = 91298
+let course; // it will give undefined
+console.log(myname,food,number,food,course
+
+);
+
+
